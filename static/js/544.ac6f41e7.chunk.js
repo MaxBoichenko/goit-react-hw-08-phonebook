1f45279cf09d798c00f2f15b19b127e801940370
MaxBoichenko <97298946+MaxBoichenko@users.noreply.github.com>
2023-02-03 04:01:21 +0000

    Deploying to gh-pages from @ MaxBoichenko/goit-react-hw-08-phonebook@fb9d20051e5ef60f7d2d01f0f9da0f0c1c19e5fc 🚀

---
+++ static/js/544.ac6f41e7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{9544:function(e,n,t){t.r(n);var o=t(266),c=t(890),h=t(184);n.default=function(){return(0,h.jsx)(o.Z,{children:(0,h.jsx)(c.Z,{component:"h2",align:"center",variant:"h2",sx:{mt:"40px"},children:"Home"})})}}}]);
+//# sourceMappingURL=544.ac6f41e7.chunk.js.map
